cb90acc44c94c4aa992a923fab7b0cce10117ca8
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2026-02-21 15:40:43 -0500

    chore(ghf): remove default config, delete type definitions, add CI workflow, and update .gitignore (#15)

--- .ghf.ts
+++ .ghf.ts
@@ -1,5 +1,3 @@
 import type { Config } from './.ghf.type'
 
-export default {
-  extends: ['https://michaelmass.github.io/ghf/ghf.default.json'],
-} satisfies Config
+export default {} satisfies Config
